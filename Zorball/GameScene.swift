1e2d21699fba5ed1802a26873ba2e3a728b73e3e
David-Mavrodiev <david_mavrodiev@abv.bg>
2017-03-29 15:30:29 -0700

    Added fire.

--- Zorball/GameScene.swift
+++ Zorball/GameScene.swift
@@ -14,8 +14,20 @@ class GameScene: SKScene {
     var previousPoint : CGPoint!
     let spriteNode = SKSpriteNode()
     
+    
     override func didMove(to view: SKView) {
+        let screen = SKEmitterNode(fileNamed: "Spark")
+        screen?.position = CGPoint(x: 0, y: 400)
+        self.addChild(screen!)
+        
         self.physicsWorld.gravity = CGVector(dx: 0, dy: -9.8)
+        let range = 1..<5
+        for i in range{
+            let fireNode = SKSpriteNode(fileNamed: "Fire")
+            fireNode?.size = CGSize(width: 1250, height: 30)
+            fireNode?.position = CGPoint(x: -450 + (i * 180), y: -700)
+            self.addChild(fireNode!)
+        }
         
         let sceneBody = SKPhysicsBody(edgeLoopFrom: self.frame)
         sceneBody.friction = 0
@@ -37,15 +49,6 @@ class GameScene: SKScene {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch: AnyObject in touches{
             previousPoint = (touch as! UITouch).location(in: self.view)
-            let positionOfTouch = touch.location(in: self)
-            let ball = SKShapeNode(circleOfRadius: 25)
-            ball.fillColor = SKColor.red
-            ball.position = positionOfTouch
-            ball.physicsBody = SKPhysicsBody(circleOfRadius: 25)
-            ball.physicsBody?.affectedByGravity = true
-            ball.physicsBody?.restitution = 1
-            ball.physicsBody?.linearDamping = 0
-            self.addChild(ball)
         }
     }
     
@@ -55,6 +58,20 @@ class GameScene: SKScene {
             let distance = currentPoint.x - previousPoint.x
             previousPoint = currentPoint
             spriteNode.zRotation = spriteNode.zRotation + distance / 100.0
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch: AnyObject in touches{
+            //let positionOfTouch = touch.location(in: self)
+            let ball = SKShapeNode(circleOfRadius: 35)
+            ball.fillColor = SKColor.red
+            ball.position = CGPoint(x: 0, y: 400)
+            ball.physicsBody = SKPhysicsBody(circleOfRadius: 35)
+            ball.physicsBody?.affectedByGravity = true
+            ball.physicsBody?.restitution = 1
+            ball.physicsBody?.linearDamping = 0
+            self.addChild(ball)
         }
     }
 }
