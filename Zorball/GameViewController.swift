01ff54748f87717dd5b4b9362481f55aa744fcb8
David-Mavrodiev <david_mavrodiev@abv.bg>
2017-03-31 16:42:22 -0700

    Added home scene.

--- Zorball/GameViewController.swift
+++ Zorball/GameViewController.swift
@@ -16,11 +16,12 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
+            //let myScene = HomeScene()
+            // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
+                //let scene = HomeScene()
+                scene.scaleMode = SKSceneScaleMode.aspectFill
                 // Present the scene
                 view.presentScene(scene)
             }
